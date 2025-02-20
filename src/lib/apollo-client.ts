832923bccc1f08a557eb11afe46e32b1a818f522
Ruslan Kolibabchuk <RKolibabchuk_c@spherasolutions.com>
2025-02-20 17:00:27 +0200

    #1600403923 - Updating types

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: `${process.env.WORDPRESS_API_URL}/graphql`,
+    //uri: `${process.env.WORDPRESS_API_URL}/graphql`,
+    uri: `https://cosmonew1.wpenginepowered.com/graphql`,
     cache: new InMemoryCache(),
 });
 
