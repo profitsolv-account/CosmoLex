3862f8a1b7de6819a04edba9ce694c22904f304a
Ruslan Kolibabchuk <RKolibabchuk_c@spherasolutions.com>
2025-02-22 23:59:06 +0200

    - Adding featured post

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import { revalidatePath } from 'next/cache';
+import {getAllPostSlugs} from "@/lib/queries/blog";
 
 export async function POST(request: any) {
     const body = await request.json();
@@ -10,6 +11,11 @@ export async function POST(request: any) {
     }
 
     try {
+        const postsSlugs = await getAllPostSlugs();
+        postsSlugs.forEach(({slug}) => {
+            revalidatePath(`/blog/${slug}`);
+        })
+
         revalidatePath(`/blog/${slug}`);
         revalidatePath(`/blog`);
         revalidatePath(`/blog/page/[page]`);
