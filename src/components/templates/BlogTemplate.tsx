47bd8b86ebaed60465b9618e399db5f6ea12cd08
Ruslan Kolibabchuk <RKolibabchuk_c@spherasolutions.com>
2025-02-26 21:15:10 +0200

    - creating Home page (adding testimonials)

--- src/components/templates/BlogTemplate.tsx
+++ src/components/templates/BlogTemplate.tsx
@@ -7,7 +7,7 @@ import Pagination from "@/components/pagination/inde";
 export default function BlogTemplate({ pageData, page }: { pageData: any, page: number }) {
     return (
         <Layout pageData={pageData}>
-            <div>
+            <div className="pb-32">
                 <section className="container mx-auto px-6 py-16 text-center">
                     <h1 className="text-5xl font-bold text-primary">{pageData?.title}</h1>
                     <p className="mt-4 text-lg text-gray-700">{pageData?.excerpt}</p>
--- src/components/templates/PostTemplate.tsx
+++ src/components/templates/PostTemplate.tsx
@@ -5,7 +5,7 @@ import Layout from "@/components/layout/layout";
 export default function PostTemplate({ pageData }: { pageData: any }) {
     return (
         <Layout pageData={pageData}>
-            <div>
+            <div className="pb-32">
                 <section className="container mx-auto px-6 py-16 text-center">
                     <h1 className="text-5xl font-bold text-primary">{pageData?.title}</h1>
                     <p className="mt-4 text-lg text-gray-700">{pageData?.excerpt}</p>
