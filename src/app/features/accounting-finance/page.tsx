a5387999abc0f2c7251ac25a03c682bed6e3428a
Ruslan Kolibabchuk <RKolibabchuk_c@spherasolutions.com>
2025-03-10 14:29:37 +0200

    - adding pillar pages

---
+++ src/app/features/accounting-finance/page.tsx
@@ -0,0 +1,30 @@
+import { Metadata } from "next";
+import { getSEOData } from "@/lib/queries/seo";
+import { getPageData } from "@/lib/queries/wordpress";
+import {getTestimonialsList} from "@/lib/queries/testimonials";
+import {notFound} from "next/navigation";
+import PillarParentTemplate from "@/components/templates/PillarParrentTemplate";
+
+const pageSlug = "accounting-finance";
+
+export async function generateMetadata(): Promise<Metadata> {
+    return await getSEOData(pageSlug);
+}
+
+export default async function PillarPage() {
+    try {
+        const pageData = await getPageData(pageSlug);
+        const testimonials = await getTestimonialsList();
+        return <PillarParentTemplate pageData={{
+            ...pageData,
+            testimonials,
+            footerExtendedBg: true
+        }} />
+    } catch (error) {
+        console.error(error);
+        notFound();
+    }
+}
+
+export const revalidate = false;
+export const dynamic = "force-static";
