e0eb45eff8e22ee4b61f4d07b67ff7ce1977afe7
Ruslan Kolibabchuk <RKolibabchuk_c@spherasolutions.com>
2025-03-13 15:12:30 +0200

    - updating pillar pages routs

--- src/components/blocks/priceComparison/index.tsx
+++ src/components/blocks/priceComparison/index.tsx
@@ -16,24 +16,39 @@ export const PriceComparison: FC<Props> = ({settings, plans, title}) => {
         <div className="max-w-[1124px] mx-auto relative z-10">
             <Heading>{title}</Heading>
 
-            <div className="bg-green py-5 px-5 rounded-[15px] shadow-[0px_21px_30px_0px_rgba(0,0,0,0.05)] md:rounded-[30px] md:py-[30px] md:px-[29px]">
+            <div
+                className="bg-green py-5 px-5 rounded-[15px] shadow-[0px_21px_30px_0px_rgba(0,0,0,0.05)] md:rounded-[30px] md:py-[30px] md:px-[29px]">
 
                 <div className="justify-start items-center gap-2.5 inline-flex w-full">
 
-                    <div className="w-[40%] text-primary-dark text-[18px] font-bold flex-none md:text-[28px] md:w-[60%]">Plans & pricing</div>
+                    <div
+                        className="w-[40%] text-primary-dark text-[18px] font-bold flex-none md:text-[28px] md:w-[60%]">Plans
+                        & pricing
+                    </div>
 
                     <div className="justify-start items-center gap-1 flex grow">
 
                         <div className="flex-col justify-start items-center inline-flex w-1/2">
-                            <div className="self-stretch text-center text-primary-dark text-[18px] font-bold md:text-2xl">Standard</div>
-                            <div className="text-primary-dark text-[23px] font-bold leading-[60px] md:text-[47px]">$99</div>
-                            <div className="self-stretch text-center text-primary-dark text-base font-normal">/ Paid annualy</div>
+                            <div
+                                className="self-stretch text-center text-primary-dark text-[18px] font-bold md:text-2xl">Standard
+                            </div>
+                            <div className="text-primary-dark text-[23px] font-bold leading-[60px] md:text-[47px]">$99
+                            </div>
+                            <div className="self-stretch text-center text-primary-dark text-base font-normal">/ Paid
+                                annualy
+                            </div>
                         </div>
 
                         <div className="flex-col justify-start items-center inline-flex w-1/2">
-                            <div className="self-stretch text-center text-primary-dark text-[18px] font-bold md:text-2xl">Elite</div>
-                            <div className="text-primary-dark text-[23px] font-bold leading-[60px] md:text-[47px]">$129</div>
-                            <div className="self-stretch text-center text-primary-dark text-base font-normal">/ Paid annualy</div>
+                            <div
+                                className="self-stretch text-center text-primary-dark text-[18px] font-bold md:text-2xl">Elite
+                            </div>
+                            <div
+                                className="text-primary-dark text-[23px] font-bold leading-[60px] md:text-[47px]">$129
+                            </div>
+                            <div className="self-stretch text-center text-primary-dark text-base font-normal">/ Paid
+                                annualy
+                            </div>
                         </div>
 
                     </div>
@@ -61,12 +76,17 @@ export const PriceComparison: FC<Props> = ({settings, plans, title}) => {
                     <div className="text-center text-primary-dark text-[28px] font-bold">Ready to dive deeper?</div>
                 </div>
                 <div className="flex gap-5  items-center flex-col sm:flex-row md:justify-center pt-10 mb-2">
-                    <a href={settings?.demoLink || ''} className="base-btn w-full md:px-7 hover:bg-primary-dark hover:text-white md:w-auto">Request demo</a>
-                    <a href={settings?.freeTrialLink || ''} className="base-btn w-full md:px-7 bg-primary-dark text-white hover:bg-transparent hover:text-primary-dark md:w-auto">Try for free</a>
+                    <a href={settings?.demoLink || ''}
+                       className="base-btn w-full md:px-7 hover:bg-primary-dark hover:text-white md:w-auto">Request
+                        demo</a>
+                    <a href={settings?.freeTrialLink || ''}
+                       className="base-btn w-full md:px-7 bg-primary-dark text-white hover:bg-transparent hover:text-primary-dark md:w-auto">Try
+                        for free</a>
                 </div>
 
             </div>
             <span className="md:!w-10 md:!h-10"></span>
+            <span className="text-[#323d42] text-[14px] font-normal pl-2 md:text-xl text-[#323d42] text-sm font-normal block"/>
         </div>
     </div>
 }
