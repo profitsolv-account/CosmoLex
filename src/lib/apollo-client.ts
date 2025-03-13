98854623d7505df1afa530201aec229f0e55a92d
Ruslan Kolibabchuk <RKolibabchuk_c@spherasolutions.com>
2025-03-13 06:24:05 +0200

    - Adding about us page

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,6 +1,17 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink, from } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+import { RetryLink } from '@apollo/client/link/retry';
 
-const client = new ApolloClient({
+const errorLink = onError(({ networkError }) => {
+    if (networkError) console.error('Network Error:', networkError);
+});
+
+const retryLink = new RetryLink({
+    attempts: (count, operation, error) => !!error && count < 3,
+    delay: (count) => Math.pow(2, count) * 1000,
+});
+
+/*const client = new ApolloClient({
     //uri: `${process.env.WORDPRESS_API_URL}/graphql`,
     uri: `https://cosmonew1.wpenginepowered.com/graphql`,
     cache: new InMemoryCache({
@@ -14,6 +25,11 @@ const client = new ApolloClient({
             fetchPolicy: 'no-cache',
         },
     }
+});*/
+
+const client = new ApolloClient({
+    link: from([errorLink, retryLink, new HttpLink({ uri: 'https://cosmonew1.wpenginepowered.com/graphql' })]),
+    cache: new InMemoryCache(),
 });
 
 export default client;
