a1203082f9753d85be1094717dc25be64621755f
Ruslan Kolibabchuk <RKolibabchuk_c@spherasolutions.com>
2025-03-13 08:43:38 +0200

    - Adding about us page

--- src/components/templates/PillarParrentTemplate.tsx
+++ src/components/templates/PillarParrentTemplate.tsx
@@ -58,38 +58,39 @@ type ToolsSectionProps = {
 }
 const ToolsSection: FC<ToolsSectionProps> = ({tools}) => {
     const items = [...tools.items, ...tools.items].map((t, index) => (
-        <Fragment key={index}>
-            <div className="h-full w-full flex flex-col-reverse justify-center lg:grid lg:grid-cols-2 overflow-hidden">
-                <div className={classNames("grow rounded-br-[15px] rounded-bl-[15px] relative bg-cover bg-center overflow-hidden h-full lg:max-h-full lg:rounded-br-[0px] lg:rounded-tl-[30px] lg:rounded-bl-[30px] lg:flex lg:items-center lg:justify-center", t.classname)}>
-                    <Image
-                        src={t.image.node.sourceUrl}
-                        alt={t.image.node.altText}
-                        className={classNames("relative z-4", t.mediaClassname)}
-                        width={t.image.node.mediaDetails?.width || 0}
-                        height={t.image.node.mediaDetails?.height || 0}
-                    />
-                    <div className="absolute z-0 top-0 left-0 w-full h-full bg-white/30"/>
-                </div>
-                <div className={classNames("grow rounded-tl-[15px] rounded-tr-[15px] relative flex items-center justify-center px-9 py-9 pb-16 lg:rounded-br-[30px] lg:rounded-tr-[30px] lg:rounded-tl-[0px] overflow-hidden", t.classname)}>
-                    <div className="lg:w-[442px] flex-col justify-start items-start gap-5 inline-flex">
-                        <div>
-                            <Image
-                                src={t.icon.node.sourceUrl}
-                                alt={t.icon.node.altText}
-                                width={t.icon.node.mediaDetails?.width || 0}
-                                height={t.icon.node.mediaDetails?.height || 0}
-                            />
-                        </div>
-                        <div className=" text-primary-dark text-[36px] font-bold leading-[45px] font-['Inter'] lg:leading-[55px] lg:text-[46px]">{t.title}</div>
-                        <div className="text-primary-dark text-base font-normal font-['Inter'] mb-2 leading-[30px] max-w-[350px] lg:mb-7" dangerouslySetInnerHTML={{ __html: t.description }} />
-                        <a href={t.link?.url || '/#'}
-                           className="w-full block text-center lg:inline-block rounded-[100px] bg-primary-dark justify-center items-center text-white text-base font-normal font-['Inter'] px-[30px] py-[15px] lg:w-auto">
-                            Explore features
-                        </a>
+    <Fragment key={index}>
+        <div className="h-full w-full flex flex-col-reverse justify-center lg:grid lg:grid-cols-2 overflow-hidden">
+            <div className={classNames("grow max-h-[280px] rounded-br-[15px] rounded-bl-[15px] relative bg-cover bg-center overflow-hidden h-full aspect-square md:aspect-auto lg:max-h-full lg:rounded-br-[0px] lg:rounded-tl-[30px] lg:rounded-bl-[30px] lg:flex lg:items-center lg:justify-center", t.classname)}>
+                <Image
+                    src={t.image.node.sourceUrl}
+                    alt={t.image.node.altText}
+                    className={classNames("relative z-4", t.mediaClassname)}
+                    width={t.image.node.mediaDetails.width}
+                    height={t.image.node.mediaDetails.height}
+                />
+                <div className="absolute z-0 top-0 left-0 w-full h-full bg-white/30"/>
+            </div>
+            <div className={classNames("grow rounded-tl-[15px] rounded-tr-[15px] relative flex items-center justify-center px-9 py-9 pb-16 lg:rounded-br-[30px] lg:rounded-tr-[30px] lg:rounded-tl-[0px] overflow-hidden", t.classname)}>
+                <div className="lg:w-[442px] flex-col justify-start items-start gap-5 inline-flex">
+                    <div>
+                        <Image
+                            src={t.icon.node.sourceUrl}
+                            alt={t.icon.node.altText}
+                            className="w-[30px] h-[30px]"
+                            width={30}
+                            height={30}
+                        />
                     </div>
+                    <div className=" text-primary-dark text-[36px] font-bold leading-[45px] font-['Inter'] lg:leading-[38px] lg:text-[30px]">{t.title}</div>
+                    <div className="text-primary-dark text-base font-normal font-['Inter'] mb-2 leading-[30px] max-w-[350px] lg:mb-7" dangerouslySetInnerHTML={{ __html: t.description }} />
+                    <a href={t.link?.url || '/#'}
+                       className="w-full block text-center lg:inline-block rounded-[100px] bg-primary-dark justify-center items-center text-white text-base font-normal font-['Inter'] px-[30px] py-[15px] lg:w-auto">
+                        Explore features
+                    </a>
                 </div>
             </div>
-        </Fragment>
+        </div>
+    </Fragment>
     ))
     const tabs: TabType[] = tools.items.map((t) => (
         {
