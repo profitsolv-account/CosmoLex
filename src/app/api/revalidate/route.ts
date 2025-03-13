5bf858b705d9e1f2e5a2b858d9477a6030d3827c
Ruslan Kolibabchuk <RKolibabchuk_c@spherasolutions.com>
2025-03-13 05:56:31 +0200

    - Adding about us page

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,18 +1,28 @@
 import { NextResponse } from 'next/server';
 import { revalidatePath } from 'next/cache';
 
-export async function POST() {
+export async function POST(req: Request) {
     try {
-        //TODO:Rewrite to handle real revalidation
-        revalidatePath(`/blog/[slug]`, "page");
-        revalidatePath(`/blog`);
-        revalidatePath(`/blog/page/[page]`, "page");
-        revalidatePath(`/[slug]`, "page");
-        revalidatePath(`/`);
-        revalidatePath(`/pricing`);
-        revalidatePath(`/features/[page]`, "page");
-        revalidatePath(`/[page]`, "page");
-        return NextResponse.json({ revalidated: true });
+
+        const body = await req.json(); // Parse JSON body
+        //console.log('Received data:', body);
+        if (body.post_type === 'page') {
+            revalidatePath(`/${body.slug}`, "page");
+            return NextResponse.json({ revalidated: true});
+        } else {
+            //TODO:Rewrite to handle real revalidation
+            revalidatePath(`/blog/[slug]`, "page");
+            revalidatePath(`/blog`);
+            revalidatePath(`/blog/page/[page]`, "page");
+            revalidatePath(`/[slug]`, "page");
+            revalidatePath(`/`);
+            revalidatePath(`/pricing`);
+            revalidatePath(`/features/[page]`, "page");
+            revalidatePath(`/[page]`, "page");
+            return NextResponse.json({ revalidated: true, body});
+        }
+
+
     } catch (err) {
         console.error(err);
         return NextResponse.json({ message: 'Error revalidating' }, { status: 500 });
