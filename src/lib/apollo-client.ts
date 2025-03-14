4d344304cd52d7b031e613cc0e76a404b42512c7
Ruslan Kolibabchuk <RKolibabchuk_c@spherasolutions.com>
2025-03-14 15:57:36 +0200

    - updating widgets

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,34 +1,27 @@
-import { ApolloClient, InMemoryCache, HttpLink, from } from '@apollo/client';
+import {
+    ApolloClient,
+    InMemoryCache,
+    HttpLink,
+    from,
+} from '@apollo/client';
 import { onError } from '@apollo/client/link/error';
 import { RetryLink } from '@apollo/client/link/retry';
 
 const errorLink = onError(({ networkError }) => {
-    if (networkError) console.error('Network Error:', networkError);
+    if (networkError) console.error(networkError);
 });
 
 const retryLink = new RetryLink({
-    attempts: (count, operation, error) => !!error && count < 3,
-    delay: (count) => Math.pow(2, count) * 1000,
+    attempts: { max: 3 },
 });
 
-/*const client = new ApolloClient({
-    //uri: `${process.env.WORDPRESS_API_URL}/graphql`,
-    uri: `https://cosmonew1.wpenginepowered.com/graphql`,
-    cache: new InMemoryCache({
-        addTypename: false,
-    }),
-    defaultOptions: {
-        query: {
-            fetchPolicy: 'no-cache',
-        },
-        watchQuery: {
-            fetchPolicy: 'no-cache',
-        },
-    }
-});*/
+const httpLink = new HttpLink({
+    uri: 'https://cosmonew1.wpenginepowered.com/graphql',
+    fetchOptions: { cache: 'force-cache' },
+});
 
 const client = new ApolloClient({
-    link: from([errorLink, retryLink, new HttpLink({ uri: 'https://cosmonew1.wpenginepowered.com/graphql' })]),
+    link: from([errorLink, retryLink, httpLink]),
     cache: new InMemoryCache(),
 });
 
