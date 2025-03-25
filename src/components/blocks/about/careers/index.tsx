67414fdeca8c6fcded6ed576b2ea2cc7ec26bd67
Ruslan Kolibabchuk <RKolibabchuk_c@spherasolutions.com>
2025-03-25 17:22:08 +0200

    - adding guides page

--- src/components/blocks/about/careers/index.tsx
+++ src/components/blocks/about/careers/index.tsx
@@ -1,52 +1,60 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export const CareersApplicantManager = () => {
-  useEffect(() => {
-    const jobBoardContainer = document.getElementById('job-board-container'); // Get the container
+    const iframeRef = useRef(null);
 
-    if (!jobBoardContainer) {
-      // Create the container if it doesn't exist
-      const newContainer = document.createElement('div');
-      newContainer.id = 'job-board-container';
-      document.body.appendChild(newContainer);
-    }
+    useEffect(() => {
+        const iframe = iframeRef.current;
+        // @ts-ignore
+        const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
 
-    const script1 = document.createElement('script');
-    script1.src = 'https://theapplicantmanager.com/jobfeeds/te_cosmolex.js';
-    document.body.appendChild(script1);
+        const htmlContent = `
+          <!DOCTYPE html>
+          <html lang="en">
+            <head>
+              <meta charset="UTF-8">
+              <title>Job Board</title>
+              <style>
+                * { 
+                    font-family: 'Arial', sans-serif; 
+                }
+                p {
+                    text-align: center;
+                }
+                </style>
+            </head>
+            <body>
+              <div id="job-board-container"></div>
+              <script src="https://theapplicantmanager.com/jobfeeds/te_cosmolex.js"></script>
+              <script src="https://theapplicantmanager.com/jobfeeds/te_footer.js"></script>
+            </body>
+          </html>
+    `;
 
-    const script2 = document.createElement('script');
-    script2.src = 'https://theapplicantmanager.com/jobfeeds/te_footer.js';
-    document.body.appendChild(script2);
+        iframeDoc.open();
+        iframeDoc.write(htmlContent);
+        iframeDoc.close();
+    }, []);
 
-    return () => {
-      // Cleanup: remove the scripts and the container (if you created it)
-      document.body.removeChild(script1);
-      document.body.removeChild(script2);
-      if(jobBoardContainer === null){
-          const containerToRemove = document.getElementById('job-board-container');
-          if(containerToRemove){
-              document.body.removeChild(containerToRemove);
-          }
-      }
+    return (
+        <div className="container mx-auto p-4">
+            <h2 className="text-5xl font-bold mb-4 text-center">Open Positions</h2>
 
-    };
-  }, []);
+            <iframe
+                ref={iframeRef}
+                title="Job Board"
+                style={{ width: '100%', height: '60px', border: 'none' }}
+            />
 
-  return (
-    <div className="container mx-auto p-4">
-      <h2 className="text-5xl font-bold mb-4 text-center">Open Positions</h2>
-
-      {/* The scripts will render their output into the 'job-board-container' */}
-
-      <div className="flex justify-center">
-        <a href="https://www.glassdoor.com/Overview/Working-at-CosmoLex-EI_IE1289709.11,19.htm">
-          <img
-            src="https://www.glassdoor.com/pc-app/static/img/partnerCenter/badges/eng_CHECK_US_273x90.png"
-            alt="Glassdoor Check Us Out"
-          />
-        </a>
-      </div>
-    </div>
-  );
+            <div className="flex justify-center mt-4">
+                <a href="https://www.glassdoor.com/Overview/Working-at-CosmoLex-EI_IE1289709.11,19.htm">
+                    <img
+                        src="https://www.glassdoor.com/pc-app/static/img/partnerCenter/badges/eng_CHECK_US_273x90.png"
+                        alt="Glassdoor Check Us Out"
+                    />
+                </a>
+            </div>
+        </div>
+    );
 };
+
