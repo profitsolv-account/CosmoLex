9a7390e5bab2a44ece9c23451b2ab34550093a19
Ruslan Kolibabchuk <RKolibabchuk_c@spherasolutions.com>
2025-03-26 16:30:22 +0200

    - adding case study pages

---
+++ src/app/canada-cheques/page.tsx
@@ -0,0 +1,28 @@
+import { Metadata } from "next";
+import { getSEOData } from "@/lib/queries/seo";
+import { getPageData } from "@/lib/queries/wordpress";
+import PageTemplate from "@/components/templates/PageTemplate";
+import {notFound} from "next/navigation";
+import "./wordpress-blocks.css";
+
+type Params = {
+    params: Promise<{slug: string}>;
+}
+
+const slug = 'canada-cheques';
+
+export async function generateMetadata({params}: Params): Promise<Metadata> {
+    return await getSEOData(slug || 'home-page');
+}
+
+export default async function SinglePage({params}: Params) {
+   try {
+       const pageData = await getPageData(slug);
+       return <PageTemplate pageData={pageData} />
+   } catch (error) {
+       notFound();
+   }
+}
+
+export const revalidate = false;
+export const dynamic = "force-static";
