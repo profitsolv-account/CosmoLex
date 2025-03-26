b61a9fee1f0c6800a54462bc7b5f80e82be8b006
Ruslan Kolibabchuk <RKolibabchuk_c@spherasolutions.com>
2025-03-26 17:34:10 +0200

    Adding case study page

--- src/components/templates/HappyCustomersTemplate.tsx
+++ src/components/templates/HappyCustomersTemplate.tsx
@@ -1,5 +1,4 @@
 import Layout from "@/components/layout/layout";
-import {HomeHeader} from "@/components/blocks/homeHeader";
 import React from "react";
 import {PageDataType} from "@/types";
 import {PageHeader} from "@/components/blocks/pageHeader";
@@ -16,13 +15,13 @@ export const HappyCustomersTemplate = ({pageData}: { pageData: PageDataType }) =
             <div className="px-4">
                 <div className="container grid grid-cols-1 md:grid-cols-2 gap-10 my-20">
                     <LinkBlock
-                        link="#"
+                        link="/case-study/jonathan-roth/"
                         title="Jonathan Roth"
                         description="Discover how Canadian lawyer Jonathan Roth eliminated the need for multiple software solutions, improved efficiency, and freed up time to focus on high-value client and business tasks with CosmoLex."
                         image={Img1.src}
                     />
                     <LinkBlock
-                        link="#"
+                        link="/case-study/kraemer-llp/"
                         title="Kraemer LLP"
                         description="Discover how Patrick Kraemer's law firm streamlined invoicing, increased client transparency, and improved project management with CosmoLex."
                         image={Img2.src}
