123f32ac449b9ebbcbc1b2e123c9dd4c488c5cb3
Ruslan Kolibabchuk <RKolibabchuk_c@spherasolutions.com>
2025-03-27 18:19:22 +0200

    adding login banner

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,13 +21,35 @@ const getRedirections = async () => {
 }
 
 export async function middleware(req: NextRequest) {
-
     const pathname = req.nextUrl.pathname;
-    const redirections = await getRedirections();
-    const redirectRule = redirections.find((r: any) => r.source === pathname);
 
-    if (redirectRule && redirectRule.status === 'enabled' && pathname !== '/') {
-        return NextResponse.redirect(new URL(redirectRule.target, req.url));
+    if (pathname.includes('_next') || pathname.includes('favicon.ico')) {
+        return NextResponse.next();
+    }
+
+    const redirections = await getRedirections();
+
+    for (const redirectRule of redirections) {
+        if (redirectRule.status !== 'enabled' || pathname === '/') continue;
+
+        if (redirectRule.source.startsWith('^')) {
+            const regex = new RegExp(redirectRule.source);
+            const match = pathname.match(regex);
+
+            if (match) {
+                let target = redirectRule.target;
+
+                if (match.length > 1) {
+                    match.slice(1).forEach((group, idx) => {
+                        target = target.replace(`$${idx + 1}`, group);
+                    });
+                }
+
+                return NextResponse.redirect(new URL(target, req.url));
+            }
+        } else if (redirectRule.source === pathname) {
+            return NextResponse.redirect(new URL(redirectRule.target, req.url));
+        }
     }
 
     return NextResponse.next();
@@ -35,5 +57,5 @@ export async function middleware(req: NextRequest) {
 
 // Apply middleware to all routes
 export const config = {
-    matcher: "/:path*", // This ensures middleware runs on all routes
+    matcher: "/:path*",
 };
