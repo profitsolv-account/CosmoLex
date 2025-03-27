d717e33501c58a15a87ab32fe5dff0d694ee06ce
Naqash Butt <naqash_9028@hotmail.com>
2025-03-27 17:16:27 +0500

    Enterprise security page

---
+++ src/app/features/enterprise-grade-security/page.tsx
@@ -0,0 +1,33 @@
+import { Metadata } from "next";
+import { getSEOData } from "@/lib/queries/seo";
+import {getPageData, getVideoSection} from "@/lib/queries/wordpress";
+import {getTestimonialsList} from "@/lib/queries/testimonials";
+import {notFound} from "next/navigation";
+import PracticeTypesTemplate from "@/components/templates//PracticeTypesTemplate";
+
+const slug = 'enterprise-grade-security';
+
+export async function generateMetadata(): Promise<Metadata> {
+    return await getSEOData(slug);
+}
+
+export default async function EnterPriseGradeSecurityPage() {
+    try {
+        const pageData = await getPageData(slug);
+        const testimonials = await getTestimonialsList();
+        const videoSection = await getVideoSection(slug);
+
+        return <PracticeTypesTemplate pageData={{
+            ...pageData,
+            testimonials,
+            footerExtendedBg: true,
+            videoSection
+        }} />
+    } catch (error) {
+        console.error(error);
+        notFound();
+    }
+}
+
+export const revalidate = false;
+export const dynamic = "force-static";
