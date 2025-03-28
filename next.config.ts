a0764c58210084d9485290b2eaad15d2cc849a37
Ruslan Kolibabchuk <RKolibabchuk_c@spherasolutions.com>
2025-03-28 20:11:02 +0200

    Adding File Sharing & eSignature Software for Legal Practices page

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,6 @@ const nextConfig: NextConfig = {
             },
         )
 
-        // Modify the file loader rule to ignore *.svg, since we have it handled now.
         fileLoaderRule.exclude = /\.svg$/i
 
         return config
