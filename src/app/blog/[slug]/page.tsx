694932a6dda4b3d378e47aa1c8827d0153356115
Ruslan Kolibabchuk <RKolibabchuk_c@spherasolutions.com>
2025-04-01 17:14:52 +0300

    1600405282 - removing system shortcodes

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -30,9 +30,14 @@ export default async function SinglePost({ params }: Params) {
        if (!pageData) {
            notFound();
        }
+
        return <PostTemplate pageData={{
            ...pageData,
-           content: formatText(pageData.content, replace),
+           content: removeSystemTags(formatText(pageData.content.replaceAll('\\n',''), replace)
+               .replaceAll('[cs_element_global_block _id=&#8221;4&#8243; ]', data.InfographicContent)
+               .replaceAll('[cs_element_global_block _id=&#8221;2&#8243; ]', data.InfographicContent)
+           )
+           ,
        }} />;
        
    } catch(error) {
@@ -40,12 +45,23 @@ export default async function SinglePost({ params }: Params) {
     }
 }
 
-const formatText = (text: string, replace: {[key: string]: string}): string => {
-    const shortcodeRegex = /\[cs_gb name=&#8221;([^&#]+)&#8221;\]/g;
+const formatText = (text: string, replace: { [key: string]: string }): string => {
+    const shortcodeRegex = /\[cs_gb name=&#8221;([^&#]+)&#8221;\]/gi; // added 'i' flag
     return text.replace(shortcodeRegex, (match, name) => {
-        return replace[name] ?? match;
+        const key = Object.keys(replace).find(k => k.toLowerCase() === name.toLowerCase());
+        return key ? replace[key] : match;
     });
 };
+
+function removeSystemTags(inputString: string) {
+    return inputString.replace(/\n/g,'').replace(/\[.*?\]/g, (match) => {
+        if (match.includes("cs_content") || match.includes("cs_element")) {
+            return "";
+        } else {
+            return match;
+        }
+    });
+}
 
 
 /*export async function generateStaticParams() {
