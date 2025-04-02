4ce2784fd2445d2cca7ab59a00a1d3363dd4db26
Ruslan Kolibabchuk <RKolibabchuk_c@spherasolutions.com>
2025-04-02 16:04:19 +0300

    1600405282 - removing system shortcodes

--- src/components/blocks/partners/index.tsx
+++ src/components/blocks/partners/index.tsx
@@ -7,14 +7,45 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
-import aba from '@/assets/img/partners/sliders/ABA.webp';
-import atl from '@/assets/img/partners/sliders/ATL.webp';
-import ls from '@/assets/img/partners/sliders/LawSites.webp';
-import ly from '@/assets/img/partners/sliders/Lawyerist.webp';
-import ltt from '@/assets/img/partners/sliders/LTT.webp';
-import canadian from '@/assets/img/partners/sliders/The-canadian-bar-association-optimized-768x432 1.webp';
 import {FC} from "react";
 import classNames from "classnames";
+
+import aaw from '@/assets/img/partners/sliders/aaw_logo.webp';
+import aba from '@/assets/img/partners/sliders/ABA.webp';
+import atl from '@/assets/img/partners/sliders/ATL.webp';
+import alabama from '@/assets/img/partners/sliders/alabama_state_bar.webp';
+import box from '@/assets/img/partners/sliders/box.webp';
+import canadian from '@/assets/img/partners/sliders/canadian_bar_assoc.webp';
+import chicago from '@/assets/img/partners/sliders/chicago_bar_assoc.webp';
+import connecticut from '@/assets/img/partners/sliders/connecticut_bar_assoc.webp';
+import copilot from '@/assets/img/partners/sliders/copilot.webp';
+import dropbox from '@/assets/img/partners/sliders/dropbox.webp';
+import florida from '@/assets/img/partners/sliders/florida_bar.webp';
+import googleWorkspace from '@/assets/img/partners/sliders/google_workspace.webp';
+import illinois from '@/assets/img/partners/sliders/illinois_state_bar_assoc.webp';
+import indiana from '@/assets/img/partners/sliders/indiana_state_bar_assoc.webp';
+import kansas from '@/assets/img/partners/sliders/kansas_bar_assoc.webp';
+import lawTechToday from '@/assets/img/partners/sliders/law_technology_today.webp';
+import lawsites from '@/assets/img/partners/sliders/lawsites.webp';
+import lawyerist from '@/assets/img/partners/sliders/lawyerist.webp';
+import lexsign from '@/assets/img/partners/sliders/lexsign_io.webp';
+import ltt from '@/assets/img/partners/sliders/LTT.webp';
+import missouri from '@/assets/img/partners/sliders/missouri_bar_member.webp';
+import msba from '@/assets/img/partners/sliders/msba.webp';
+import nacba from '@/assets/img/partners/sliders/national_assoc_consumer_bankruptcy.webp';
+import netdocuments from '@/assets/img/partners/sliders/netdocuments.webp';
+import newJersey from '@/assets/img/partners/sliders/new_jersey_state_bar_assoc.webp';
+import oklahoma from '@/assets/img/partners/sliders/oklahoma_bar_assoc.webp';
+import outlook from '@/assets/img/partners/sliders/outlook.webp';
+import philadelphia from '@/assets/img/partners/sliders/philadelphia_bar_assoc.webp';
+import plf from '@/assets/img/partners/sliders/professional_liability_fund.webp';
+import rhodeIsland from '@/assets/img/partners/sliders/rhode_island_bar_assoc.webp';
+import southCarolina from '@/assets/img/partners/sliders/south_carolina_bar.webp';
+import stateGeorgia from '@/assets/img/partners/sliders/state_bar_georgia.webp';
+import stateArizona from '@/assets/img/partners/sliders/state_bar_of_arizona.webp';
+import stateTexas from '@/assets/img/partners/sliders/state_bar_texas.webp';
+import washington from '@/assets/img/partners/sliders/washington_state_bar_assoc.webp';
+
 
 type Props = {
     className?: string;
@@ -22,13 +53,43 @@ type Props = {
 
 export const Partners: FC<Props> = ({className}) => {
     const items = [
+        aaw.src,
         aba.src,
         atl.src,
-        ltt.src,
+        alabama.src,
+        box.src,
         canadian.src,
-        ly.src,
-        ls.src,
+        chicago.src,
+        connecticut.src,
+        copilot.src,
+        dropbox.src,
+        florida.src,
+        googleWorkspace.src,
+        illinois.src,
+        indiana.src,
+        kansas.src,
+        lawTechToday.src,
+        lawsites.src,
+        lawyerist.src,
+        lexsign.src,
+        ltt.src,
+        missouri.src,
+        msba.src,
+        nacba.src,
+        netdocuments.src,
+        newJersey.src,
+        oklahoma.src,
+        outlook.src,
+        philadelphia.src,
+        plf.src,
+        rhodeIsland.src,
+        southCarolina.src,
+        stateGeorgia.src,
+        stateArizona.src,
+        stateTexas.src,
+        washington.src,
     ];
+
 
     // Duplicate items multiple times for a smooth infinite loop
     const slides = [...items, ...items];
@@ -55,8 +116,14 @@ export const Partners: FC<Props> = ({className}) => {
                     >
                         {slides.map((src, index) => (
                             <SwiperSlide key={index} style={{ width: '12.6875rem' }}>
-                                <div className="bg-white rounded-[0.625rem] flex justify-center items-center overflow-hidden w-[12.6875rem] h-[7.5625rem]">
-                                    <img className="object-contain" src={src} alt={`Slide ${index}`} />
+                                <div className={classNames("bg-white rounded-[0.625rem] flex justify-center items-center overflow-hidden w-[12.6875rem] h-[7.5625rem] p-3", {
+                                    '!p-0': src === connecticut.src,
+                                    '!p-8': src === box.src,
+                                })}>
+                                    <img
+                                        className="object-contain max-h-full"
+                                        src={src}
+                                        alt={`Slide ${index}`} />
                                 </div>
                             </SwiperSlide>
                         ))}
