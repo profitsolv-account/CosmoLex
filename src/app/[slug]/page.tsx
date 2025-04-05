12ddea4b68e52c7e7bae0712875eae79a738af45
Ruslan Kolibabchuk <RKolibabchuk_c@spherasolutions.com>
2025-04-05 09:38:12 +0300

    Adding gtm

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -4,20 +4,38 @@ import { getPageData } from "@/lib/queries/wordpress";
 import PageTemplate from "@/components/templates/PageTemplate";
 import {notFound} from "next/navigation";
 import "./wordpress-blocks.css";
+import {getLanguage} from "@/lib/helpers";
 
 type Params = {
     params: Promise<{slug: string}>;
 }
 
+const getSlug = async (defaultSlug: string) => {
+    const lang = await getLanguage();
+    let slug = defaultSlug
+    switch (lang) {
+        case 'ca':
+            slug = `${defaultSlug}-2`;
+            break;
+        case 'uk':
+            slug = `${defaultSlug}-3`;
+            break;
+    }
+    return slug;
+}
+
 export async function generateMetadata({params}: Params): Promise<Metadata> {
     const {slug} = await params;
-    return await getSEOData(slug);
+    const newSlug = await getSlug(slug);
+    return await getSEOData(newSlug);
 }
 
 export default async function SinglePage({params}: Params) {
    try {
        const {slug} = await params;
-       const pageData = await getPageData(slug);
+       const newSlug = await getSlug(slug);
+
+       const pageData = await getPageData(newSlug);
        return <PageTemplate pageData={pageData} />
    } catch (error) {
        notFound();
@@ -25,4 +43,4 @@ export default async function SinglePage({params}: Params) {
 }
 
 export const revalidate = false;
-export const dynamic = "force-static";
+
