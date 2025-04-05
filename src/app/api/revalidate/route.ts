877e8494e5a26f03daab884cb0b5541ad1de3b1c
Ruslan Kolibabchuk <RKolibabchuk_c@spherasolutions.com>
2025-04-05 19:26:16 +0300

    Adding gtm

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 import {revalidatePath, revalidateTag} from 'next/cache';
 import client from "@/lib/apollo-client";
 
+const REVALIDATE_SECRET='rev-token-122'
+
 const baseUrl = process.env.BASE_URL || 'https://cosmonew1.wpenginepowered.com/';
 const getPagePath = (url: string) => {
     return new URL(url, baseUrl).pathname;
@@ -9,6 +11,13 @@ const getPagePath = (url: string) => {
 
 export async function POST(req: Request) {
     try {
+
+        const { searchParams } = new URL(req.url);
+        const token = searchParams.get('secret');
+
+        if (token !== REVALIDATE_SECRET) {
+            return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+        }
 
         const body = await req.json();
 
