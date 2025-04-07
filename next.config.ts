bc502a4d0a5bed91b750b291ae2f43086fc5c783
Ruslan Kolibabchuk <RKolibabchuk_c@spherasolutions.com>
2025-04-07 18:02:06 +0300

    Updating next config to use api from env

--- next.config.ts
+++ next.config.ts
@@ -1,52 +1,52 @@
 import type { NextConfig } from "next";
+import dotenv from "dotenv";
+
+// Load env variables from .env files
+dotenv.config();
+
+const WORDPRESS_API_URL = process.env.WORDPRESS_API_URL;
 
 const nextConfig: NextConfig = {
-
     webpack(config) {
-        // Grab the existing rule that handles SVG imports
         const fileLoaderRule = config.module.rules.find((rule: any) =>
             rule.test?.test?.('.svg'),
-        )
+        );
 
         config.module.rules.push(
-            // Reapply the existing rule, but only for svg imports ending in ?url
             {
                 ...fileLoaderRule,
                 test: /\.svg$/i,
                 resourceQuery: /url/, // *.svg?url
             },
-            // Convert all other *.svg imports to React components
             {
                 test: /\.svg$/i,
                 issuer: fileLoaderRule.issuer,
-                resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+                resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
                 use: ['@svgr/webpack'],
             },
-        )
+        );
 
-        fileLoaderRule.exclude = /\.svg$/i
+        fileLoaderRule.exclude = /\.svg$/i;
 
-        return config
+        return config;
     },
 
-    // rest settings
     output: 'standalone',
     trailingSlash: true,
     reactStrictMode: true,
+
     images: {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'cosmonew1.wpenginepowered.com',
+                hostname: new URL(WORDPRESS_API_URL!).hostname,
                 port: '',
                 pathname: '/wp-content/uploads/**',
             },
         ],
     },
-    /*images: {
-        unoptimized: true,
-    },*/
-    allowedDevOrigins: ['*', 'cl.com']
+
+    allowedDevOrigins: ['*', 'cl.com'],
 };
 
 export default nextConfig;
