d8d372726c64316a483c4c66917613cabff0a609
Ruslan Kolibabchuk <RKolibabchuk_c@spherasolutions.com>
2025-05-06 22:06:00 +0300

    Updating blog page

--- src/components/resources/singleBlock/index.tsx
+++ src/components/resources/singleBlock/index.tsx
@@ -2,8 +2,8 @@ import {CustomLink} from "@/components/ui/customLink";
 
 
 export const SingleBlock = () => {
-    return <div className="inline-flex justify-start items-start gap-4 flex-wrap content-start">
-        <div className="flex-1 flex justify-between items-center">
+    return <div className="inline-flex justify-start items-start gap-4 flex-wrap content-start flex-col">
+        <div className="flex-1 flex justify-between items-center w-full">
             <div data-type="Article" className="h-3 relative">
                 <div className="left-0 top-0 justify-start text-primary-dark text-base font-normal uppercase tracking-wider relative">ARTICLE</div>
             </div>
@@ -14,7 +14,9 @@ export const SingleBlock = () => {
                 View All
             </CustomLink>
         </div>
-        <img className="rounded-[1.25rem]" src="https://placehold.co/267x170" />
+        <div className="w-full">
+            <img className="rounded-[1.25rem] w-full" src="https://placehold.co/267x170" />
+        </div>
         <div className="flex-1 inline-flex flex-col justify-start items-start gap-4">
             <div className="self-stretch justify-start text-primary-dark text-xl font-bold leading-[2rem]">Law Firm Partnership Structure: Understanding Roles and Responsibilities</div>
             <div className="self-stretch justify-start text-primary-dark text-base font-normal leading-[1.875rem]">Law firm partnerships can be as complicated as the cases they handle.</div>
